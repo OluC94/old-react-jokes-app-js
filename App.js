a33935913ff17080f4e8dd4bb7eea43bbc75a129
OluC94 <coker.olu94@gmail.com>
2024-06-14 11:53:06 +0100

    implement map

--- App.js
+++ App.js
@@ -2,11 +2,33 @@ import React from "react"
 import Joke from "./Components/Joke"
 
 const App = () => {
+    const jokeData = [
+        {
+            setup: "How would you describe a male ant that floats?",
+            punchline: "Bouyant"
+        },
+        {
+            setup: "",
+            punchline: "I got back from the hospital and the doctor told us that my wife is eating for two now (she has a tapeworm)"
+        },
+        {
+            setup: "How many software engineers does it take to change a lightbulb",
+            punchline: "Three. One to hold the lightbulb and two to spin the ladder"
+        },
+        {
+            setup: "",
+            punchline: "Time flies like an arrow. Fruit flies like a banana"
+        },
+        {
+            setup: "A policy officer caught two kids playing with a firework and a car battery.",
+            punchline: "He charged one and let the other off."
+        }
+        
+    ]
     return (<main>
-        <Joke setup="What's the best thing about Switzerland?" punchline="I don't know, but the flag is a big plus!"/>
-        <Joke setup="How would you describe a male ant that floats?" punchline="Bouyant"/>
-        <Joke punchline="I got back from the hospital and the doctor told us that my wife is eating for two now (she has a tapeworm)"/>
-        <Joke setup="How many software engineers does it take to change a lightbulb" punchline="Three. One to hold the lightbulb and two to spin the ladder"/>
+        {jokeData.map(joke => {
+            return (<Joke setup={joke.setup} punchline={joke.punchline} />)
+        })}
     </main>)
 }
 
--- index.pack.js
+++ index.pack.js
@@ -405,13 +405,28 @@ var _Joke2 = _interopRequireDefault(_Joke);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var App = function App() {
+    var jokeData = [{
+        setup: "How would you describe a male ant that floats?",
+        punchline: "Bouyant"
+    }, {
+        setup: "",
+        punchline: "I got back from the hospital and the doctor told us that my wife is eating for two now (she has a tapeworm)"
+    }, {
+        setup: "How many software engineers does it take to change a lightbulb",
+        punchline: "Three. One to hold the lightbulb and two to spin the ladder"
+    }, {
+        setup: "",
+        punchline: "Time flies like an arrow. Fruit flies like a banana"
+    }, {
+        setup: "A policy officer caught two kids playing with a firework and a car battery.",
+        punchline: "He charged one and let the other off."
+    }];
     return _react2.default.createElement(
         "main",
         null,
-        _react2.default.createElement(_Joke2.default, { setup: "What's the best thing about Switzerland?", punchline: "I don't know, but the flag is a big plus!" }),
-        _react2.default.createElement(_Joke2.default, { setup: "How would you describe a male ant that floats?", punchline: "Bouyant" }),
-        _react2.default.createElement(_Joke2.default, { punchline: "I got back from the hospital and the doctor told us that my wife is eating for two now (she has a tapeworm)" }),
-        _react2.default.createElement(_Joke2.default, { setup: "How many software engineers does it take to change a lightbulb", punchline: "Three. One to hold the lightbulb and two to spin the ladder" })
+        jokeData.map(function (joke) {
+            return _react2.default.createElement(_Joke2.default, { setup: joke.setup, punchline: joke.punchline });
+        })
     );
 };
 
